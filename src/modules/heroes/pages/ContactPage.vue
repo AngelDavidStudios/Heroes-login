d9f34882d200a75c2b83534fd43aa9c88c5c4736
Angel David <angeldavidstudios@outlook.com>
2025-04-03 17:15:39 -0500

    Arreglado bug de atributos obsoletos de tailwind

--- src/modules/heroes/pages/ContactPage.vue
+++ src/modules/heroes/pages/ContactPage.vue
@@ -7,11 +7,7 @@
     <div class="absolute inset-0 bg-gray-300">
       <iframe width="100%"
               height="100%"
-              frameborder="0"
-              marginheight="0"
-              marginwidth="0"
               title="map"
-              scrolling="no"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9621.835300078965!2d-78.50019840382595!3d-0.22424109222463584!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91d599f4bb3f257f%3A0x37b802a439557013!2sItchimb%C3%ADa%2C%20Quito!5e0!3m2!1ses-419!2sec!4v1743449380056!5m2!1ses-419!2sec" style=""></iframe>
     </div>
     <div class="container px-5 py-24 mx-auto flex">
