fb249cb431976717b024d5f2851c6dfcb771d0b4
Angel David <angeldavidstudios@outlook.com>
2025-04-03 12:27:18 -0500

    Predefincion de rutas para CRuD de heroes y Auth

---
+++ src/router/index.ts
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HeroesPage from '@/modules/heroes/pages/HeroesPage.vue'
+
+
+const router = createRouter({
+  history: createWebHistory( import.meta.env.BASE_URL ),
+  routes: [
+    {
+      path: '/',
+      name: 'LandingHeroes',
+      component: HeroesPage
+    }
+  ]
+})
+
+export default router
