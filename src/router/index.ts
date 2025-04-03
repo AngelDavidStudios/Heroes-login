81257c1980939a4e3842ee21878eeda6bf7aaa18
Angel David <angeldavidstudios@outlook.com>
2025-04-03 14:47:39 -0500

    Definicion de las rutas y creacion de la pagina para el error 404

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HeroesPage from '@/modules/heroes/pages/HeroesPage.vue'
+import AuthLanding from '@/modules/auth/layouts/AuthLanding.vue'
+import LoginPage from '@/modules/auth/pages/LoginPage.vue'
+import RegisterPage from '@/modules/auth/pages/RegisterPage.vue'
+import NotFound404 from '@/modules/common/pages/NotFound404.vue'
 
 
 const router = createRouter({
@@ -15,7 +19,28 @@ const router = createRouter({
 
     //Auth
     {
+      path: '/auth',
+      name: 'LandingAuth',
+      component: AuthLanding,
+      children: [
+        {
+          path: 'login',
+          name: 'Login',
+          component: LoginPage
+        },
+        {
+          path: 'register',
+          name: 'Register',
+          component: RegisterPage
+        }
+      ]
+    },
 
+    // Not Found
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'NotFound',
+      component: NotFound404
     }
   ]
 })
